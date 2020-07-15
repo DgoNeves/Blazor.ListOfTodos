8fce64bd752758f8f4733869bdb5c8970811fd9d
Diogo Neves <kidix7@gmail.com>
2020-07-15 22:24:45 +0100

    Add project files.

---
+++ ListOfTodos/Client/Components/TodoComponent.razor.cs
@@ -0,0 +1,20 @@
+﻿using ListOfTodos.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListOfTodos.Client.Components
+{
+    public class TodoComponentBase : ComponentBase
+    {
+        [Parameter]
+        public List<TodoItem> TodoItems { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+    }
+}
---
+++ ListOfTodos/Shared/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListOfTodos.Shared
+{
+    public class TodoItem
+    {
+        public string Name { get; set; }
+
+        public bool IsDone { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+    }
+}
