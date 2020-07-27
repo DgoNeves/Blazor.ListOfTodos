b0d9af396b755d80c2319ca56eb0346360a7aee5
Diogo Neves <kidix7@gmail.com>
2020-07-27 22:01:50 +0100

    Added TodoRepository and DI

---
+++ ListOfTodos/Server/Repos/TodoRepository.cs
@@ -0,0 +1,13 @@
+﻿using ListOfTodos.Shared;
+using OakIdeas.GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListOfTodos.Server.Repos
+{
+    public class TodoRepository :  MemoryGenericRepository<TodoItem>, IGenericRepository<TodoItem>
+    {
+    }
+}
--- ListOfTodos/Server/Startup.cs
+++ ListOfTodos/Server/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using OakIdeas.GenericRepository;
+using ListOfTodos.Shared;
+using ListOfTodos.Server.Repos;
 
 namespace ListOfTodos.Server
 {
@@ -25,6 +28,8 @@ namespace ListOfTodos.Server
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddScoped<IGenericRepository<TodoItem>, TodoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
