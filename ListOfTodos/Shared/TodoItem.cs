3ae5e7b87ecad66b63b04ef472601e654923429a
Diogo Neves <kidix7@gmail.com>
2020-07-27 14:54:02 +0100

    Added todoItem to shared

--- ListOfTodos/Shared/TodoItem.cs
+++ ListOfTodos/Shared/TodoItem.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿using OakIdeas.GenericRepository.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ListOfTodos.Shared
 {
-    public class TodoItem
+    public class TodoItem : EntityBase
     {
-        public string Name { get; set; }
+        public string TaskName { get; set; }
 
         public bool IsDone { get; set; }
 
-        public DateTime CreateDate { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
 
         public DateTime DueDate { get; set; }
     }
